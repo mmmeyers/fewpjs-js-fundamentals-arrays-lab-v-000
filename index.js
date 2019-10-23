5afbb9223946ab735b71b748e7e1211ecde11592
Marsi Meyers-Miyashita <marsi286@gmail.com>
2019-10-23 02:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push('Odie');
